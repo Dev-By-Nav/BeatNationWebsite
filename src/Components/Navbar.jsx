eb5a86a63a3645f8e4e439685472908a2c2a7484
Navneet <itsnav3@gmail.com>
2023-11-01 19:58:18 +0000

    third commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
-import { motion } from 'framer-motion';
 import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
 import { Link } from 'react-router-dom';
 import logo from '../assets/logo2.png';
+import { motion } from 'framer-motion';
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
@@ -11,23 +11,18 @@ const Navbar = () => {
     setNav(!nav);
   };
 
-  const navbarVariants = {
-    hidden: { opacity: 0, x: '-100vw' },
-    visible: { opacity: 1, x: 0 },
-  };
-
-  const linkVariants = {
-    hidden: { opacity: 0, x: '100vw' },
-    visible: { opacity: 1, x: 0 },
+  const logoVariants = {
+    hidden: { opacity: 0, x: -50 },
+    visible: { opacity: 1, x: 0, transition: { duration: 1.5 } }, // Adjust duration as needed
   };
 
   const lineVariants = {
-    hidden: { width: '50%', left: '25%' },
-    visible: { width: '100%', left: '0%' },
+    hidden: { width: '0%', left: '50%' },
+    visible: { width: '100%', left: '0%', transition: { duration: 2 } }, // Adjust duration as needed
   };
 
   return (
-    <motion.nav initial='hidden' animate='visible' variants={navbarVariants}>
+    <nav>
       <div
         className={`flex justify-between items-center h-28 min-w-full mx-auto px-12 text-white  ${
           nav ? 'sticky top-0' : ''
@@ -38,84 +33,50 @@ const Navbar = () => {
             src={logo}
             alt=''
             className='h-36 w-auto'
-            initial={{ opacity: 0, x: '-100vw' }}
-            animate={{ opacity: 1, x: 0 }}
-            transition={{ duration: 1 }}
+            initial='hidden'
+            animate='visible'
+            variants={logoVariants}
           />
         </Link>
-        <ul
-          className={`hidden md:flex font-semibold cursor-pointer text-bn-gold text-lg ${
-            nav ? 'hidden' : ''
-          }`}
-        >
-          <motion.li
-            className='p-4 hover:text-white font-medium'
-            variants={linkVariants}
-          >
+        <ul className={`hidden md:flex font-semibold cursor-pointer text-bn-gold text-lg ${nav ? 'hidden' : ''}`}>
+          <li className='p-4 hover:text-white font-medium'>
             <Link to='/'>Home</Link>
-          </motion.li>
-          <motion.li
-            className='p-4 hover:text-white font-medium'
-            variants={linkVariants}
-          >
+          </li>
+          <li className='p-4 hover:text-white font-medium'>
             <Link to='/gallery'>Gallery</Link>
-          </motion.li>
-          <motion.li
-            className='p-4 hover:text-white font-medium'
-            variants={linkVariants}
-          >
+          </li>
+          <li className='p-4 hover:text-white font-medium'>
             <Link to='/services'>Services</Link>
-          </motion.li>
-
-          <motion.li
-            className='p-4 hover:text-white font-medium flex-row'
-            variants={linkVariants}
-          >
+          </li>
+         
+          <li className='p-4 hover:text-white font-medium flex-row'>
             <Link to='/contact-us'>Contact Us</Link>
-          </motion.li>
+          </li>
         </ul>
-        <div
-          onClick={handleNav}
-          className='block md:hidden cursor-pointer text-[#d4af37] hover:text-[#ad345c] mr-4'
-        >
-          {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+        <div onClick={handleNav} className='block md:hidden cursor-pointer text-[#d4af37] hover:text-[#ad345c] mr-4'>
+          {nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20}/>}
         </div>
-        <motion.div
+        <div
           className={`${
-            nav
-              ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-bn-navy ease-in-out duration-500 md:hidden'
-              : 'fixed left-[-100vw] z-50'
+            nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-bn-navy ease-in-out duration-500 md:hidden' : 'fixed left-[-100%] z-50'
           }`}
-          variants={navbarVariants}
         >
-          <motion.ul className='p-4 cursor-pointer text-[#d4af37] font-semibold'>
-            <motion.li
-              className='p-4 border-b hover:text-white border-bn-gold font-medium'
-              variants={linkVariants}
-            >
+          <ul className='p-4 cursor-pointer text-[#d4af37] font-semibold'>
+            <li className='p-4 border-b hover:text-white border-bn-gold font-medium'>
               <Link to='/'>Home</Link>
-            </motion.li>
-            <motion.li
-              className='p-4 border-b hover:text-white border-bn-gold font-medium'
-              variants={linkVariants}
-            >
+            </li>
+            <li className='p-4 border-b hover:text-white border-bn-gold font-medium'>
               <Link to='/gallery'>Gallery</Link>
-            </motion.li>
-            <motion.li
-              className='p-4 border-b hover:text-white border-bn-gold font-medium'
-              variants={linkVariants}
-            >
+            </li>
+            <li className='p-4 border-b hover:text-white border-bn-gold font-medium'>
               <Link to='/services'>Services</Link>
-            </motion.li>
-
-            <motion.li
-              className='p-4 hover:text-white font-medium flex-row'
-              variants={linkVariants}
-            >
+            </li>
+            
+            <li className='p-4 hover:text-white font-medium flex-row'>
               <Link to='/contact-us'>Contact Us</Link>
-            </motion.li>
-          </motion.ul>
-        </motion.div>
+            </li>
+          </ul>
+        </div>
       </div>
       <motion.hr
         className='text-bn-gold'
@@ -129,9 +90,8 @@ const Navbar = () => {
         initial='hidden'
         animate='visible'
         variants={lineVariants}
-        transition={{ duration: 2 }} // Set your desired animation duration
       />
-    </motion.nav>
+    </nav>
   );
 };
 
